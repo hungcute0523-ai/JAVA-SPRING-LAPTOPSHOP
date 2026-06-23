0053aeb0e69440e0267b654a0ee7b7760e2b82de
Hung Coder <hungcute0523@gmail.com>
2026-06-23 16:56:07 +0700

    up load

---
+++ src/test/java/com/Hung/java_spring_laptopshop_starter/JavaSpringLaptopshopStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Hung.java_spring_laptopshop_starter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSpringLaptopshopStarterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
